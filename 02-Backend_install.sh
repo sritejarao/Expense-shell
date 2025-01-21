6807e3aa5a6d858e5a7f82b4c4a104dc503525e8
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-21 08:31:50 +0530

    Automaing Mysql install

---
+++ 02-Backend_install.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+day=$(date +%y-%m-%d)
+TIME_STAMP=$(date +%y-%m-%d-%H-%M-%S)
+PATH1="/var/log/script_logs"
+LOGFILENAME="$PATH1/$LOG_FILE-$day.log"
+mkdir -p $PATH1
+
+VALIDATE()
+{
+    if [ $? -ne 0 ]; then
+        echo "$2 ..... Failure"
+        exit 1
+    else
+        echo " $2 ....... Success"
+    fi
+}
+
+if [ $USER -ne 0 ]; then
+    echo "To install mysql user should have root access"
+    exit 1
+else 
+    echo "This user can only install mysql service"
+fi
+
+
+dnf module disable nodejs -y &>>$LOGFILENAME
+VALIDATE $? "nodejs:20 disable"
+
+dnf module enable nodejs:20 -y  &>>$LOGFILENAME
+VALIDATE $? "nodejs:20 enable"
+
+dnf install nodejs -y  &>>$LOGFILENAME
+if [$? -ne 0 ] then
+    VALIDATE $? "nodejs installed"
+else
+    VALIDATE $? "nodejs installed"
+fi
+
+id expense
+if [ $? -ne 0 ]; then
+    useradd expense  &>>$LOGFILENAME
+    VALIDATE $? "expense user added"
+else
+    VALIDATE $? "expense user already present"
+fi
+
+cd /app  &>>$LOGFILENAME
+if [ $? -ne 0]
+then
+    mkdir -p /app
+    VALIDATE $? "app directory created"
+else
+    VALIDATE $? "app directory already present"
+fi
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip  &>>$LOGFILENAME
+VALIDATE $? "download"
+
+cd /app
+VALIDATE $? "directory access"
+
+rm -rf *  &>>$LOGFILENAME
+VALIDATE $? "Old files removed"
+
+unzip /tmp/backend.zip  &>>$LOGFILENAME
+VALIDATE $? "Unzipping backend service"
+
+dnf list installed npm   &>>$LOGFILENAME
+if [ $? -ne 0 ]; then
+    echo "npm install started"
+    dnf install npm -y  &>>$LOGFILENAME
+    if [ $? -ne 0 ]; then
+        VALIDATE $? "npm install"
+    else
+        VALIDATE $? "npm install"
+    fi
+else
+    VALIDATE $? "npm already installed"
+fi
+
+cp /home/ec2-user/backend.service /etc/systemd/system/backend.service  &>>$LOGFILENAME
+ll /etc/systemd/system/backend.service  &>>$LOGFILENAME
+if [$? -ne 0 ]; then
+    VALIDATE $? "backend.service not present"
+else
+    VALIDATE $? "backend.service is present"
+fi
+systemctl daemon-reload  &>>$LOGFILENAME
+VALIDATE $? "daemon-reload"
+
+systemctl start backend  &>>$LOGFILENAME
+VALIDATE $? "Backend started"
+
+systemctl enable backend  &>>$LOGFILENAME
+VALIDATE $? "Backend enabled"
+
+dnf list installed mysql  &>>$LOGFILENAME
+if [ $? -ne 0 ]; then
+    dnf install mysql -y  &>>$LOGFILENAME
+    VALIDATE $? "MySql install"
+else
+    VALIDATE $? "Mysql already installed"
+fi
+mysql -h mysql.learnnewthings.site -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$LOGFILENAME
+VALIDATE $? "backend.sql loading"
+
+systemctl restart backend  &>>$LOGFILENAME
+VALIDATE $? "backend restart"
